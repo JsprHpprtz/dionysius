91cfd4d017fb8df76da8c5b30dd164239d1ca407
JasperHuppertz <127968342+JasperHuppertz@users.noreply.github.com>
2025-05-26 13:24:39 +0200

    test commit

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,41 @@
+<footer class="footer sm:footer-horizontal bg-base-200 text-base-content p-10">
+  <aside>
+    <div>
+        <img src="images/logo.png" class="w-full max-w-[3rem] sm:max-w-[4rem] md:max-w-[5rem] h-auto" alt="logo">
+    </div>
+
+    
+    <p>
+        Dionysius
+      <br />
+      © 2017. Alle rechten voorbehouden. — Dionysius
+    </p>
+  </aside>
+  <nav>
+    <h6 class="footer-title">Contactgegevens bestuur</h6>
+    <a href="https://maps.app.goo.gl/g4Pctw4XEaa1viKg9" target="_blank" rel="noopener noreferrer" class="underline hover:text-primary">
+        Westerpark 80
+        <p>5042 MH Tilburg</p>
+    </a>
+    <p>bestuur@dionysius.nl</p>
+  </nav>
+  <nav>
+    <h6 class="footer-title">Contactgegevens ledenadministratie</h6>
+    <a href="https://maps.app.goo.gl/pB9ZtKXDuTRkxcaa7" target="_blank" rel="noopener noreferrer" class="underline hover:text-primary">
+        Herwijnenstraat 43
+        <p>5045 GP Tilburg</p>
+    </a>
+    <p>ledenadministratie@dionysius.nl</p>
+  </nav>
+  <nav>
+    <h6 class="footer-title">Dionysius is aangesloten bij:</h6>
+    <div class="">
+        <img src="images/footer-banner.png" class="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg h-auto" alt="Footer banner">
+    </div>
+  </nav>
+  <nav>
+    <h6 class="footer-title">Rekeningnummer contributie</h6>
+    <p>IBAN: NL 36 INGB 000 375 9771</p>
+    <p>BIC: INGBNL2A</p>
+  </nav>
+</footer>
